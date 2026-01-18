125fa0787df6b44b7446517035413298f57c8ef2
unknown <madawa.dulanda55@gmail.com>
2026-01-18 22:32:40 +0530

    update

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import os
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev-secret-key')
 
 # Portfolio data - customize this for your client
 PORTFOLIO_DATA = {
@@ -207,6 +209,5 @@ def contact():
 
 
 if __name__ == '__main__':
-    import os
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
